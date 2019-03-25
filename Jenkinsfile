67ae8385ec2edc1768dc347315d4218899f5786a
zadigus <laurent.michel@softozor.ch>
2019-03-25 11:54:24 +0100

    test usage of deploy-to-jelastic script

---
+++ Jenkinsfile
@@ -0,0 +1,19 @@
+pipeline {
+  agent {
+    docker {
+      image 'everpeace/curl-jq'
+    }
+  } 
+  stages {
+    stage('Test') {
+      environment {
+        HIDORA_CREDENTIALS = credentials('hidora')
+        ENVIRONMENT_NAME = "jenkins-ci-test"
+      }
+      steps {
+        sh 'chmod u+x ./deploy-to-jelastic.sh'
+        sh "./deploy-to-jelastic.sh $HIDORA_CREDENTIALS_USR $HIDORA_CREDENTIALS_PSW $ENVIRONMENT_NAME"
+      }
+    }
+  }
+}
--- deploy-to-jelastic.sh
+++ deploy-to-jelastic.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
+
+if [ $# -ne 3 ] ; then
+  echo "Usage: $0 login password envName [deploy_group = cp]"
+fi
+
 HOSTER_URL="https://app.hidora.com"
-ENV_NAME=${3:-demonstration}
+ENV_NAME=$3
 APPID="1dd8d191d38fff45e62564fcf67fdcd6"
 CONTENT_TYPE="Content-Type: application/x-www-form-urlencoded; charset=UTF-8;";
 USER_AGENT="Mozilla/4.73 [en] (X11; U; Linux 2.2.15 i686)"
@@ -13,7 +18,6 @@ echo 'Response signIn user: '$signIn
 
 echo "SignIn...2";
 
-RESULT=$(jq '.result' <<< $signIn );
 SESSION=$( jq '.session' <<< $signIn |  sed 's/\"//g' );
 
 echo "Check if env is created...";
@@ -22,26 +26,28 @@ envs=$(curl -k \
 -A "${USER_AGENT}" \
 -X POST \
 -fsS ${HOSTER_URL}/1.0/environment/control/rest/getenvs -d "appid=${APPID}&session=${SESSION}");
-CREATED=$(echo $envs | jq '[.infos[].env.envName]' | jq "contains([\"$ENV_NAME\"])")
+ENVNAMES=$(echo $envs | jq '[.infos[].env.envName]')
+CREATED=$(echo $envs | jq '[.infos[].env.envName]' | jq "index(\"$ENV_NAME\")")
 
-if [[ "${CREATED}" = "true" ]]; then
-  echo "Redeploy existing environment (Group ${DEPLOY_GROUP})..."
-  redeploy=$(curl -k \
-  -H "${CONTENT_TYPE}" \
-  -A "${USER_AGENT}" \
-  -X POST \
-  -fsS ${HOSTER_URL}/1.0/environment/control/rest/redeploycontainersbygroup \
-  -d "appid=${APPID}&session=${SESSION}&envName=${ENV_NAME}&tag=latest&nodeGroup=${DEPLOY_GROUP}&useExistingVolumes=true&delay=20");
-  echo "Environment redeployed"
-else
+# TODO: need to check whether the environment is up and running in order to deploy???
+if [[ "${CREATED}" == "null" ]]; then
   echo "Install new environment...";
-  MANIFEST=$(cat manifest.jps)
-  installApp=$(curl -k \
-  -A "${USER_AGENT}" \
-  -H "${CONTENT_TYPE}" \
-  -X POST -fsS ${HOSTER_URL}"/1.0/development/scripting/rest/eval" \
-  --data "session=${SESSION}&shortdomain=${ENV_NAME}&envName=${ENV_NAME}&script=InstallApp&appid=appstore&type=install&charset=UTF-8" --data-urlencode "manifest=$MANIFEST");
-  echo "Response install env: "$installApp;
+  #MANIFEST=$(cat manifest.jps)
+  #installApp=$(curl -k \
+  #-A "${USER_AGENT}" \
+  #-H "${CONTENT_TYPE}" \
+  #-X POST -fsS ${HOSTER_URL}"/1.0/development/scripting/rest/eval" \
+  #--data "session=${SESSION}&shortdomain=${ENV_NAME}&envName=${ENV_NAME}&script=InstallApp&appid=appstore&type=install&charset=UTF-8" --data-urlencode "manifest=$MANIFEST");
+  #echo "Response install env: "$installApp;
+else
+  echo "Redeploy existing environment (Group ${DEPLOY_GROUP})..."
+  #redeploy=$(curl -k \
+  #-H "${CONTENT_TYPE}" \
+  #-A "${USER_AGENT}" \
+  #-X POST \
+  #-fsS ${HOSTER_URL}/1.0/environment/control/rest/redeploycontainersbygroup \
+  #-d "appid=${APPID}&session=${SESSION}&envName=${ENV_NAME}&tag=latest&nodeGroup=${DEPLOY_GROUP}&useExistingVolumes=true&delay=20");
+  #echo "Environment redeployed"
 fi
 
 exit 0
