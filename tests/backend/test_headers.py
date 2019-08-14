deabfbbc375661bccddcb37ff5f7a78be5311241
Laurent Michel <laurent.michel@softozor.ch>
2019-08-14 15:11:55 +0200

    integrate security headers tests

--- tests/backend/test_headers.py
+++ tests/backend/test_headers.py
@@ -12,3 +12,9 @@ def test_ip_resolver_not_returned(graphql_endpoint, query_me):
     res = requests.post(graphql_endpoint, json=query_me)
     with pytest.raises(KeyError):
         res.headers["X-Resolver-IP"]
+
+
+def test_security_headers_need_highest_possible_grade(domain):
+    res = requests.get('https://securityheaders.com/?q=' +
+                       domain + '&followRedirects=on')
+    assert res.headers['X-Grade'] == 'A+'
