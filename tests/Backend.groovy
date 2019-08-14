e11129db4dd2f8d523f07391e3ff6e36594307f2
Laurent Michel <laurent.michel@softozor.ch>
2019-08-14 10:21:29 +0200

    initial backend test pipeline

---
+++ tests/Backend.groovy
@@ -0,0 +1,32 @@
+pipeline {
+  agent any
+  environment {
+    REPORTS_FOLDER = "$WORKSPACE/tests/junit-reports"
+    VENV = 'venv'
+  }
+  stages {
+    stage('Virtual Environment Installation') {
+      steps {
+        withEnv(["HOME=$WORKSPACE"]) {
+          sh "virtualenv $VENV"
+          sh ". $VENV/bin/activate"
+          sh ". $VENV/bin/activate && pip install -r tests/requirements.txt"
+        }
+      }
+    }
+    stage('Security Tests') {
+        steps {
+            withEnv(["HOME=$WORKSPACE"]) {
+                sh ". $VENV/bin/activate && cd tests/backend && pytest --domain $DOMAIN --graphql-endpoint graphql/ -ra --junitxml=$REPORTS_FOLDER/backend-tests.xml"
+            }
+        }
+    }
+  }
+  post {
+    always {
+      script {
+         junit "$REPORTS_FOLDER/*.xml"
+      }
+    }
+  }
+}
