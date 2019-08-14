f4121b03bea88574e793d705b42ac0a673cc4c79
Laurent Michel <laurent.michel@softozor.ch>
2019-08-14 11:16:44 +0200

    fix wake up stage

--- tests/Backend.groovy
+++ tests/Backend.groovy
@@ -22,10 +22,12 @@ pipeline {
     }
     stage('Wake up Jelastic environment') {
         steps {
-            def SCRIPT_TO_RUN = './scripts/wake-up-env.sh'
-            sh "dos2unix ./scripts/helpers.sh"
-            sh "dos2unix $SCRIPT_TO_RUN"
-            sh "$SCRIPT_TO_RUN $JELASTIC_APP_CREDENTIALS_USR $JELASTIC_APP_CREDENTIALS_PSW $JELASTIC_CREDENTIALS_USR $JELASTIC_CREDENTIALS_PSW $HIDORA_DOMAIN"
+            script {
+                SCRIPT_TO_RUN = './scripts/wake-up-env.sh'
+                sh "dos2unix ./scripts/helpers.sh"
+                sh "dos2unix $SCRIPT_TO_RUN"
+                sh "$SCRIPT_TO_RUN $JELASTIC_APP_CREDENTIALS_USR $JELASTIC_APP_CREDENTIALS_PSW $JELASTIC_CREDENTIALS_USR $JELASTIC_CREDENTIALS_PSW $HIDORA_DOMAIN"
+            }
         }
     }
     stage('Security Tests') {
