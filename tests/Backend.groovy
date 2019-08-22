ed6f9c672a70eab8f6819fa180449a50f023eea1
shikamu <tomy4ever@gmail.com>
2019-08-22 13:45:52 +0200

    added the common folder to PYTHONPATH
    so that we can import modules from tests/common

--- tests/Backend.groovy
+++ tests/Backend.groovy
@@ -18,6 +18,9 @@ pipeline {
         }
     }
     stage('Security Tests') {
+        environment {
+          PYTHONPATH = "$PYTHONPATH:$WORKSPACE/tests/common"
+        }    
         steps {
             withEnv(["HOME=$WORKSPACE"]) {
                 sh "cd tests/backend && pytest --domain http://${HIDORA_DOMAIN}.hidora.com --graphql-endpoint graphql/ -ra --junitxml=backend-tests.xml"
@@ -33,3 +36,6 @@ pipeline {
     }
   }
 }
+
+
+
